ea29ab232245c1f4da67b81b5b15e5918b164cc0
JuanNeftaly <00182222@uca.edu.sv>
2023-10-26 18:30:13 -0600

    fix: Rename

---
+++ Basic React/First-Component/src/Componentes/App.jsx
@@ -0,0 +1,13 @@
+/* COMPONENTES EN REACT - JSX */
+// importar libreria
+import React from "react";
+
+// creating component with class
+class App extends React.Component {
+  render() {
+    return <p>Hola soy un componete de clase</p>;
+  }
+}
+
+// export is very important
+export default App;
