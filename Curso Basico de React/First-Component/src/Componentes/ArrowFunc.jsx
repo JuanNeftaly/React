03af9f9a72c21e33b36ef406e7b6ff2ff49bf944
Juan Neftaly Castellanos Hernández <104848289+JuanNeftaly@users.noreply.github.com>
2023-10-26 18:32:29 -0600

    del: outdated file

--- Curso Basico de React/First-Component/src/Componentes/ArrowFunc.jsx
+++
@@ -1,6 +0,0 @@
-// arrow function component
-const Apli = () => {
-  return <p>Soy un componete de funcion flecha</p>;
-};
-
-export default Apli;
--- Curso Basico de React/First-Component/src/Componentes/Function.jsx
+++
@@ -1,6 +0,0 @@
-// create function
-function Aplicate() {
-  return <p>Hola soy un componete de funcion</p>;
-}
-
-export default Aplicate;
