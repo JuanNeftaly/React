f12b2120f438e335aa6357839b670a7546c47c5b
JuanNeftaly <00182222@uca.edu.sv>
2023-10-27 17:27:06 -0600

    add: Hooks - useState

---
+++ Basic React/Hooks/src/components/contador.jsx
@@ -0,0 +1,30 @@
+// importando Hooks
+import React, {useState} from "react";
+
+const Contador = () => {
+  // contador valor actual
+  // setContador la funcion para actualizar valor
+  const [contador, setContador] = useState(0);
+  return (
+    <div>
+      <h1>Haz clickeado {contador}</h1>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        Dame Click :D
+      </button>
+
+      <button
+        onClick={() => {
+          setContador(0);
+        }}
+      >
+        Reset :0
+      </button>
+    </div>
+  );
+};
+
+export default Contador;
