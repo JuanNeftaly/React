da63512944f0d5df614ba48efd100c810f7c6e49
JuanNeftaly <00182222@uca.edu.sv>
2023-10-26 18:37:35 -0600

    add: learning props

--- Basic React/First-Component/src/Componentes/Father.jsx
+++ Basic React/First-Component/src/Componentes/Father.jsx
@@ -1,8 +1,14 @@
+// learning props
+const Text = () => {
+  return <p>Soy un componete papu</p>;
+};
+
 const Father = () => {
   return (
     <div>
       <p>Hijo primero del componente padre</p>
       <p>Hijo segundo del componente padre</p>
+      <Text />
     </div>
   );
 };
