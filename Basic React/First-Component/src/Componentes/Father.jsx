87ec75dd754ea57314413d86e6a2e8732bfd54ae
JuanNeftaly <00182222@uca.edu.sv>
2023-10-26 18:42:36 -0600

    props in functions

--- Basic React/First-Component/src/Componentes/Father.jsx
+++ Basic React/First-Component/src/Componentes/Father.jsx
@@ -1,14 +1,17 @@
 // learning props
-const Text = () => {
+// this function gets the prop
+const Text = (propiedad) => {
+  console.log(propiedad);
   return <p>Soy un componete papu</p>;
 };
 
+// "name" is the props that we send
 const Father = () => {
   return (
     <div>
       <p>Hijo primero del componente padre</p>
       <p>Hijo segundo del componente padre</p>
-      <Text />
+      <Text name="Juan Neftaly" lastname="Castellanos" />
     </div>
   );
 };
