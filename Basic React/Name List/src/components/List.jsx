b110aa32ffbfc027b3a55434fdb70b40804b0d59
JuanNeftaly <00182222@uca.edu.sv>
2023-10-27 20:43:38 -0600

    add: input to name list

--- Basic React/Name List/src/components/List.jsx
+++ Basic React/Name List/src/components/List.jsx
@@ -4,13 +4,34 @@ import React, { useState } from "react";
 const List = () => {
   // variables de estado
   const [name, setName] = useState("");
+  // sirve para modificarse
   const [names, setNames] = useState([]);
 
+  // funcion de guardar
+  const save = () => {
+    // guardar en arreglo
+    setNames([...names, name]);
+    setName("");
+  };
+
+  // cosas a retornar
   return (
     <div>
       <p>Ingresa un nombre</p>
-      <input type="text" value={name} onChange={() => {}} />
-      <button>Enviar</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button onClick={save}>Enviar</button>
+
+      <ul>
+        {names.map((elemento, index) => {
+          return <li key={index}> {elemento}</li>;
+        })}
+      </ul>
     </div>
   );
 };
