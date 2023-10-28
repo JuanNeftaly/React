3689512ee6c785cea47ce177b3e62949089b5cbd
JuanNeftaly <00182222@uca.edu.sv>
2023-10-27 21:21:50 -0600

    add: increase and decrease counters

--- Basic React/Hooks/src/components/contador.jsx
+++ Basic React/Hooks/src/components/contador.jsx
@@ -6,6 +6,10 @@ const Contador = () => {
   // setContador la funcion para actualizar valor
   const [contador, setContador] = useState(0);
 
+  // to do: crear un contador para cada useEffect para saber cuantas veces se clickeo cada uno
+  const [contAdd, setContAdd] = useState(0);
+  const [contDel, setContDel] = useState(0);
+
   // aplicando useEffect
   useEffect(() => {
     // todo lo que se ejecuta
@@ -18,21 +22,32 @@ const Contador = () => {
     console.log("contador: ", { contador });
   }, [contador]);
 
+  // contando cuantas se aumenta y disminuye
+  useEffect(() => {
+    console.log("Aumento: ", { contAdd });
+  }, [contAdd]);
+
+  useEffect(() => {
+    console.log("Disminuye: ", { contDel });
+  }, [contDel]);
+
   return (
     <div>
       <h1> Haz clickeado {contador}</h1>
       <button
         onClick={() => {
           setContador(contador + 1);
+          setContAdd(contAdd + 1);
         }}
       >
-        Dame Click :D
+        Aumentar
       </button>
 
       <button
         onClick={() => {
           if (contador > 0) {
             setContador(contador - 1);
+            setContDel(contDel + 1);
           }
         }}
       >
