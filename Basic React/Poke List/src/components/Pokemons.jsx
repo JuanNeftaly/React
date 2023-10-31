449d15b02371532baa153d4c6099cf4d86d5dcde
JuanNeftaly <00182222@uca.edu.sv>
2023-10-30 23:39:54 -0600

    add: next & previous buttons

--- Basic React/Poke List/src/components/Pokemons.jsx
+++ Basic React/Poke List/src/components/Pokemons.jsx
@@ -7,19 +7,28 @@ const Pokemons = () => {
     // variable para guardar pokemons
     const [pokemones, setPokemons] = useState([]);
 
+    // variables para funcion de botones
+    const [anterior, setAnterior] = useState(null);
+    const [siguiente, setSiguiente] = useState(null);
+    const [actual, setActual] = useState(
+        "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10&offset=0"
+    );
+
     // llamado a la API
     useEffect(() => {
-
         // creando funcion asincrona
         async function getPokemons() {
-
             try {
-                const response = await axios.get(
-                    "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10"
-                );
+                const response = await axios.get(actual);
+
                 // Axios devuelve los datos en la propiedad 'data'
                 const data = response.data;
                 setPokemons(data.results);
+
+                // dandole funcion a anterior - siguiente
+                setAnterior(data.previous);
+                setSiguiente(data.next);
+
             } catch (error) {
                 // Manejo de errores en caso de que falle la petición
                 console.error("Hubo un error al obtener los pokemones", error);
@@ -27,17 +36,26 @@ const Pokemons = () => {
         }
 
         getPokemons();
-    }, []);
+    }, [actual]);
 
     return (
-        <ul>
-            {
-                // mapear resultados
-                pokemones.map((pokemon, index) => {
-                    return <li key={index}> {pokemon.name} </li>;
-                })
-            }
-        </ul>
+        <div>
+            <ul>
+                {
+                    // mapear resultados
+                    pokemones.map((pokemon, index) => {
+                        return <li key={index}> {pokemon.name} </li>;
+                    })
+                }
+            </ul>
+
+            <button onClick={() => anterior !== null && setActual(anterior)}>
+                Anterior
+            </button>
+            <button onClick={() => siguiente !== null && setActual(siguiente)}>
+                Siguiente
+            </button>
+        </div>
     );
 };
 
