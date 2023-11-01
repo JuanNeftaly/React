99f7a9ba2de0c6ac67d0e7e0c7144e581d27d600
JuanNeftaly <00182222@uca.edu.sv>
2023-11-01 15:52:03 -0600

    add: Flexbox with Tailwind

---
+++ Tailwind/Flexbox/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="font-mono">Hola</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
