33e054b3e98df8cba49e3176ed5459f8f36d124b
JuanNeftaly <00182222@uca.edu.sv>
2023-10-31 21:31:06 -0600

    add: tailwind practice

---
+++ Tailwind/Test Taildwind/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center align-middle bg-black'>
+        <button className='bg-blue-500 hover:bg-blue-750 text-white font-bold py-2 px-4 rounded'>Boton</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
