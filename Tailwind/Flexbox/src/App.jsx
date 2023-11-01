c6872fcd4974dc75c27445e380b61767a976a94a
JuanNeftaly <00182222@uca.edu.sv>
2023-11-01 16:42:47 -0600

    add: tailwind style

--- Tailwind/Flexbox/src/App.jsx
+++ Tailwind/Flexbox/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Box from "./components/Box";
 
 function App() {
 
   return (
     <>
-      <div className="flex justify-center">
-        <h1 className="font-mono">Hola</h1>
+      <div className="flex justify-center bg-green-500">
+        <Box />
+        <Box />
+        <Box />
       </div>
     </>
   );
---
+++ Tailwind/Flexbox/src/components/Box.jsx
@@ -0,0 +1,13 @@
+// creando componente caja 
+const Box = () => {
+
+    return (
+        // plano de la caja
+        <div className="m-5 flex items-center justify-center rounded bg-red-600 w-20 h-20 text-center ">
+            <p className="font-mono">Caja</p>
+        </div>
+    )
+
+}
+
+export default Box;
