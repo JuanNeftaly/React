6a2a367d35c9daba3cd9c10b63535b89ac5cce76
JuanNeftaly <00182222@uca.edu.sv>
2023-10-31 21:41:33 -0600

    adding class to buttons

--- Tailwind/Test Taildwind/src/App.jsx
+++ Tailwind/Test Taildwind/src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-
   return (
     <>
-      <div className='flex justify-center align-middle bg-black'>
-        <button className='bg-blue-500 hover:bg-blue-750 text-white font-bold py-2 px-4 rounded'>Boton</button>
+      <div className="flex justify-center align-middle bg-black space-x-4">
+        <button className="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
+          Boton
+        </button>
+        <button className="bg-blue-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+          Boton
+        </button>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
